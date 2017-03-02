8d1e62aaa34f4356dc2cb565dcac1d7fd3d09e5b
Corey Shrader <corey.shrader@yahoo.com>
2017-03-01 22:42:55 -0500

    Print board, and static data members

--- src/TTTClient.java
+++ src/TTTClient.java
@@ -4,24 +4,19 @@
 
 import java.io.*;
 import java.net.*;
-import java.util.*;
-
 
 public class TTTClient {
-    static char[][] board = {{' ', ' ', ' '},
-                             {' ', ' ', ' '},
-                             {' ', ' ', ' '}};
-    static DataInputStream instream;
-    static DataOutputStream outstream;
-    static Socket toserversocket;
-    static int   reply;   // for later use
+
+    static private char[][] board = {{' ', ' ', ' '},
+                                    {' ', ' ', ' '},
+                                    {' ', ' ', ' '}};
+    static private DataInputStream instream;
+    static private DataOutputStream outstream;
+    static private Socket toserversocket;
+    static private int reply;   // for later use
 
     public static void main( String[] args)
     {
-
-
-
-
 
         System.out.println("CLIENT is attempting connection....");
 
@@ -49,7 +44,7 @@ public class TTTClient {
                 board[0][0],board[0][1],board[0][2],board[1][0],board[1][1],board[1][2],board[2][0],board[2][1],board[2][2]);
     }
 
-    public static void localUD(int r, int c, char m){// local board update with chosen move
+    public static void update(int r, int c, char m){// local board update with chosen move
         //move is always row-column
         //assumes that board has been checked for open spot
         board[r][c] = m;
