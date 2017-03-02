237b3427c708541d1d2e42d3b1833b900080001e
cwgroppe <groppec1@webmail.nku.edu>
2017-03-01 22:01:37 -0500

    localUP -> local update method for updating board

--- src/TTTClient.java
+++ src/TTTClient.java
@@ -4,13 +4,13 @@
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
+
 
 public class TTTClient {
-
     static char[][] board = {{' ', ' ', ' '},
-            {' ', ' ', ' '},
-            {' ', ' ', ' '}};
-
+                             {' ', ' ', ' '},
+                             {' ', ' ', ' '}};
     static DataInputStream instream;
     static DataOutputStream outstream;
     static Socket toserversocket;
@@ -18,6 +18,10 @@ public class TTTClient {
 
     public static void main( String[] args)
     {
+
+
+
+
 
         System.out.println("CLIENT is attempting connection....");
 
@@ -40,8 +44,10 @@ public class TTTClient {
         catch (IOException  e) {};
     }
 
-    public static void printBoard() {
-        System.out.printf("\n  %c  |  %c  |  %c  \n_________________\n  %c  |  %c  |  %c  \n_________________\n  %c  |  %c  |  %c  \n\n",
-                board[0][0],board[0][1],board[0][2],board[1][0],board[1][1],board[1][2],board[2][0],board[2][1],board[2][2]);
+    public static void localUD(int r, int c, char m){// local board update with chosen move
+        //move is always row-column
+        //assumes that board has been checked for open spot
+        board[r][c] = m;
     }
 }
+
